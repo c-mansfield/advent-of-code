8b31d3af79071ef1a523b145b93eca2334b07b9e
cole-mansfield <colemansfield@gmail.com>
2022-12-05 12:39:58 +0000

    Day 1 solved

---
+++ 22/python/day01/day01.py
@@ -0,0 +1,7 @@
+elves = [i.split('\n') for i in open("day01/input.txt", "r").read().split('\n\n')]
+
+# Part 1
+print("Part 1: ", max([sum(map(int, elf)) for elf in elves]))
+
+# Part 2
+print("Part 2: ", sum(sorted([sum(map(int, elf)) for elf in elves])[-3:]))
